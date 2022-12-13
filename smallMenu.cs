5b6190138ce55f7356162d0c37e8efceb4a51e1e
Al-Jazzazi <120479148+Al-Jazzazi@users.noreply.github.com>
2022-12-13 18:45:53 +0400

    Update smallMenu.cs

--- smallMenu.cs
+++ smallMenu.cs
@@ -1,3 +1,9 @@
+/*
+Small menu is added to the forest and desert scenes in case the user wants to go back,
+Instead of having to go back by quitting the program and running it again, we choose 
+to have the smallMenu in each scene 
+*/
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,10 +11,11 @@ public class smallMenu : MonoBehaviour
 {
 
     [SerializeField]
+    //Initilzing an instance object 
     private GameObject pausemenu;
 
 
-
+//Two buttons, one for loading the main menu scene and the other for quitting the program
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
